76610b266558be557515405fe82f383f9e237d7c
Manuel Reina <manuel.reina@hotmail.com>
2015-10-29 21:44:08 +0100

    Form submit complete

--- MiniApp/app.js
+++ MiniApp/app.js
@@ -3,29 +3,26 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-var _data = [
-    { author: "Pete Hunt", text: "This is one commenttttt" },
-    { author: "Jordan Walke", text: "This is another comment" }
-];
 var CommentForm = (function (_super) {
     __extends(CommentForm, _super);
-    function CommentForm() {
-        _super.apply(this, arguments);
+    function CommentForm(props, context) {
+        _super.call(this, props, context);
+        this.state = { name: '', text: '' };
     }
-    //handleSubmit(e: React.FormEvent): React.FormEventHandler {
-    //    e.preventDefault();
-    //    var author = this.refs["author"];
-    //    var text = this.refs["text"];
-    //    if (!text || !author) {
-    //        return;
-    //    }
-    //    // TODO: send request to the server
-    //    this.refs["author"] = '';
-    //    this.refs["author"] = '';
-    //    return;
-    //}
+    CommentForm.prototype.handleSubmit = function (e) {
+        e.preventDefault();
+        var name = this.refs["name"];
+        var text = this.refs["text"];
+        if (!text || !name) {
+            return;
+        }
+        this.props.onCommentSubmit({ author: name["value"], text: text["value"] });
+        return;
+    };
     CommentForm.prototype.render = function () {
-        return (React.createElement("form", {"className": "commentForm"}, React.createElement("input", {"type": "text", "placeholder": "Your name"}), React.createElement("input", {"type": "text", "placeholder": "Say something...", "ref": "text"}), React.createElement("input", {"type": "submit", "value": "Post"})));
+        var name = this.state.name;
+        var text = this.state.text;
+        return (React.createElement("form", {"className": "commentForm", "onSubmit": this.handleSubmit.bind(this)}, React.createElement("input", {"type": "text", "placeholder": "Your name", "ref": "name"}), React.createElement("input", {"type": "text", "placeholder": "Say something...", "ref": "text"}), React.createElement("input", {"type": "submit", "value": "Post"})));
     };
     return CommentForm;
 })(React.Component);
@@ -45,20 +42,31 @@ var CommentList = (function (_super) {
         _super.apply(this, arguments);
     }
     CommentList.prototype.render = function () {
-        var comentList = this.props.data.map(function (comment) {
-            return React.createElement(CommentItem, {"author": comment.author}, comment.text);
+        var comentListt = this.props.data.map(function (comment, i) {
+            return React.createElement(CommentItem, {"key": i, "author": comment.author}, comment.text);
         });
-        return React.createElement("div", {"className": "commentList"}, comentList);
+        return React.createElement("div", {"className": "commentList"}, comentListt);
     };
     return CommentList;
 })(React.Component);
 var CommentBox = (function (_super) {
     __extends(CommentBox, _super);
-    function CommentBox() {
-        _super.apply(this, arguments);
+    function CommentBox(props, context) {
+        _super.call(this, props, context);
+        this._data = [
+            { author: "Pete Hunt", text: "This is one commenttttt" },
+            { author: "Jordan Walke", text: "This is another comment" }
+        ];
+        this.state = { data: this._data };
     }
+    CommentBox.prototype.handleCommentSubmit = function (comment) {
+        // TODO: submit to the server and refresh the list
+        var currentList = this.state["data"];
+        currentList.push({ author: comment.author, text: comment.text });
+        this.setState({ data: currentList });
+    };
     CommentBox.prototype.render = function () {
-        return React.createElement("div", {"className": "commentBox"}, React.createElement("h1", null, "Comments"), React.createElement(CommentList, {"data": _data}), React.createElement(CommentForm, null));
+        return React.createElement("div", {"className": "commentBox"}, React.createElement("h1", null, "Comments"), React.createElement(CommentList, {"data": this._data}), React.createElement(CommentForm, {"onCommentSubmit": this.handleCommentSubmit.bind(this)}));
     };
     return CommentBox;
 })(React.Component);
--- MiniApp/app.tsx
+++ MiniApp/app.tsx
@@ -2,37 +2,45 @@
     author: string;
     text: string;
 }
-var _data: ICommentData[] = [
-    { author: "Pete Hunt", text: "This is one commenttttt" },
-    { author: "Jordan Walke", text: "This is another comment" }
-];
+
 
 interface ICommentForm extends React.Props<any> {
-    //refs: 
+    onCommentSubmit: any;
 }
 
 
 class CommentForm extends React.Component<ICommentForm, any> {
 
-    //handleSubmit(e: React.FormEvent): React.FormEventHandler {
-    //    e.preventDefault();
-    //    var author = this.refs["author"];
-    //    var text = this.refs["text"];
-    //    if (!text || !author) {
-    //        return;
-    //    }
+    constructor(props: ICommentForm, context) {
+        super(props, context);
         
-    //    // TODO: send request to the server
-    //    this.refs["author"] = '';
-    //    this.refs["author"] = '';
-    //    return;
-    //}
+        this.state = { name: '', text: '' };
+    }
+    
+
+    handleSubmit(e: React.FormEvent): React.FormEventHandler {
+        e.preventDefault();
+        var name = this.refs["name"];
+        var text = this.refs["text"];
+
+        if (!text || !name) {
+            return;
+        }
+
+        this.props.onCommentSubmit({ author: name["value"], text: text["value"] });
+        
+        
+        return;
+    }
 
     public render() {
+        var name = this.state.name;
+        var text = this.state.text; 
+
         return (
-            <form className="commentForm">
-                <input type="text" placeholder="Your name" />
-                <input type="text" placeholder="Say something..." ref="text" />
+            <form className="commentForm" onSubmit={this.handleSubmit.bind(this) }>
+                <input type="text" placeholder="Your name" ref="name"/>
+                <input type="text" placeholder="Say something..."  ref="text"/>
                 <input type="submit" value="Post" />
             </form>
         );
@@ -64,27 +72,47 @@ class CommentList extends React.Component<ICommentList, any> {
 
     public render() {
 
-        var comentList = this.props.data.map(function (comment) {
-            return <CommentItem author={comment.author}>{comment.text}</CommentItem>
+        var comentListt = this.props.data.map(function (comment, i) {
+            return <CommentItem key={i} author={comment.author}>{comment.text}</CommentItem>
         });
 
         return <div className="commentList">
-                    {comentList}
+                    {comentListt}
                </div>;
     }
 }
 
 interface ICommentBox extends React.Props<any>{
+    
 }
 
 class CommentBox extends React.Component<ICommentBox, any> {
 
+    _data: ICommentData[] = [
+        { author: "Pete Hunt", text: "This is one commenttttt" },
+        { author: "Jordan Walke", text: "This is another comment" }
+    ];
+
+    constructor(props: ICommentBox, context) {
+        super(props, context);
+        
+        this.state = { data: this._data };
+    }
+
+    handleCommentSubmit(comment) {
+        // TODO: submit to the server and refresh the list
+        var currentList = this.state["data"];
+        currentList.push({ author: comment.author, text: comment.text });
+        this.setState({ data: currentList });
+    }
+
     public render() {
+
 
         return <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={ _data } />
-                <CommentForm />
+                <CommentList data={ this._data } />
+                <CommentForm onCommentSubmit={ this.handleCommentSubmit.bind(this) }/>
                </div>;
     }
 }
