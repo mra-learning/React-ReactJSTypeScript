052470256aeec82771d8c6e603c1a404ca9548d1
mra_git <manuel.reina@hotmail.com>
2015-10-29 17:58:26 +0100

    Form submit - pending

--- MiniApp/app.js
+++ MiniApp/app.js
@@ -3,19 +3,64 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-var HelloMessage = (function (_super) {
-    __extends(HelloMessage, _super);
-    function HelloMessage() {
+var _data = [
+    { author: "Pete Hunt", text: "This is one commenttttt" },
+    { author: "Jordan Walke", text: "This is another comment" }
+];
+var CommentForm = (function (_super) {
+    __extends(CommentForm, _super);
+    function CommentForm() {
         _super.apply(this, arguments);
     }
-    HelloMessage.prototype.render = function () {
-        var helloList = this.props.data.map(function (item) {
-            return (React.createElement("li", null, "item"));
-        });
-        return React.createElement("ul", null, " ", helloList, " ");
+    //handleSubmit(e: React.FormEvent): React.FormEventHandler {
+    //    e.preventDefault();
+    //    var author = this.refs["author"];
+    //    var text = this.refs["text"];
+    //    if (!text || !author) {
+    //        return;
+    //    }
+    //    // TODO: send request to the server
+    //    this.refs["author"] = '';
+    //    this.refs["author"] = '';
+    //    return;
+    //}
+    CommentForm.prototype.render = function () {
+        return (React.createElement("form", {"className": "commentForm"}, React.createElement("input", {"type": "text", "placeholder": "Your name"}), React.createElement("input", {"type": "text", "placeholder": "Say something...", "ref": "text"}), React.createElement("input", {"type": "submit", "value": "Post"})));
     };
-    return HelloMessage;
+    return CommentForm;
 })(React.Component);
-var list = ['a', 'b', 'c'];
-React.render(React.createElement(HelloMessage, {"data": list}), document.getElementById('content'));
+var CommentItem = (function (_super) {
+    __extends(CommentItem, _super);
+    function CommentItem() {
+        _super.apply(this, arguments);
+    }
+    CommentItem.prototype.render = function () {
+        return React.createElement("div", {"className": "comment"}, React.createElement("h2", {"className": "commentAuthor"}, this.props.author), this.props.children);
+    };
+    return CommentItem;
+})(React.Component);
+var CommentList = (function (_super) {
+    __extends(CommentList, _super);
+    function CommentList() {
+        _super.apply(this, arguments);
+    }
+    CommentList.prototype.render = function () {
+        var comentList = this.props.data.map(function (comment) {
+            return React.createElement(CommentItem, {"author": comment.author}, comment.text);
+        });
+        return React.createElement("div", {"className": "commentList"}, comentList);
+    };
+    return CommentList;
+})(React.Component);
+var CommentBox = (function (_super) {
+    __extends(CommentBox, _super);
+    function CommentBox() {
+        _super.apply(this, arguments);
+    }
+    CommentBox.prototype.render = function () {
+        return React.createElement("div", {"className": "commentBox"}, React.createElement("h1", null, "Comments"), React.createElement(CommentList, {"data": _data}), React.createElement(CommentForm, null));
+    };
+    return CommentBox;
+})(React.Component);
+React.render(React.createElement(CommentBox, null), document.getElementById('content'));
 //# sourceMappingURL=app.js.map
--- MiniApp/app.tsx
+++ MiniApp/app.tsx
@@ -1,21 +1,93 @@
-﻿interface IHelloMessage {
-    data: string[];
+﻿interface ICommentData {
+    author: string;
+    text: string;
+}
+var _data: ICommentData[] = [
+    { author: "Pete Hunt", text: "This is one commenttttt" },
+    { author: "Jordan Walke", text: "This is another comment" }
+];
+
+interface ICommentForm extends React.Props<any> {
+    //refs: 
 }
 
-class HelloMessage extends React.Component<IHelloMessage, any> { 
+
+class CommentForm extends React.Component<ICommentForm, any> {
+
+    //handleSubmit(e: React.FormEvent): React.FormEventHandler {
+    //    e.preventDefault();
+    //    var author = this.refs["author"];
+    //    var text = this.refs["text"];
+    //    if (!text || !author) {
+    //        return;
+    //    }
+        
+    //    // TODO: send request to the server
+    //    this.refs["author"] = '';
+    //    this.refs["author"] = '';
+    //    return;
+    //}
 
     public render() {
-
-        var helloList = this.props.data.map(function (item) { 
-            return (
-                <li>item</li>
-            );
-        });
-
-        return <ul> { helloList } </ul>;
+        return (
+            <form className="commentForm">
+                <input type="text" placeholder="Your name" />
+                <input type="text" placeholder="Say something..." ref="text" />
+                <input type="submit" value="Post" />
+            </form>
+        );
     }
 }
 
-var list = ['a', 'b', 'c'];
+interface ICommentItem extends React.Props<any> {
+    author: string;
+}
 
-React.render(<HelloMessage data = { list } />, document.getElementById('content')); 
+class CommentItem extends React.Component<ICommentItem, any> {
+
+    public render() {
+
+        return <div className="comment">
+                <h2 className="commentAuthor">
+                  {this.props.author}
+                    </h2>
+                {this.props.children}
+               </div>;
+    }
+}
+
+interface ICommentList extends React.Props<any>{
+    data: ICommentData[];
+}
+
+class CommentList extends React.Component<ICommentList, any> {
+
+    public render() {
+
+        var comentList = this.props.data.map(function (comment) {
+            return <CommentItem author={comment.author}>{comment.text}</CommentItem>
+        });
+
+        return <div className="commentList">
+                    {comentList}
+               </div>;
+    }
+}
+
+interface ICommentBox extends React.Props<any>{
+}
+
+class CommentBox extends React.Component<ICommentBox, any> {
+
+    public render() {
+
+        return <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={ _data } />
+                <CommentForm />
+               </div>;
+    }
+}
+
+
+React.render(<CommentBox />, document.getElementById('content')); 
